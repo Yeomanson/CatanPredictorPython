84f36cb45fe0057e7b457f8354218dec9133a233
King Man <ryanjking42@gmail.com>
2018-11-21 15:16:51 -0500

    Rebranded the Solution file to be Django based

---
+++ CatanWebApp/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Ryan\Desktop\Catan Project\CatanPredictorPython\CatanWebApp\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
